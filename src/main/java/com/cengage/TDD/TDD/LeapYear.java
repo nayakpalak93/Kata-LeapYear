fbbb4f4d9aa4d15cca544b5d4f16b35d48cc2058
Palak Ashokbhai Nayak <nayakpalak93@gmail.com>
2017-06-24 15:00:49 -0400

    LeapYear

---
+++ src/main/java/com/cengage/TDD/TDD/LeapYear.java
@@ -0,0 +1,62 @@
+package com.cengage.TDD.TDD;
+import java.util.Scanner;
+
+public class LeapYear 
+{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		String year = sc.nextLine();
+		if(checkIfLeapYear(year)){
+			System.out.println("Year is Leap Year");
+		}else{
+			System.out.println("Year is not a Leap Year");	
+		}	
+	}
+	public static boolean checkIfInputIsInteger(String numberToCheck) {
+		boolean result = false;
+		try{
+			Integer.parseInt(numberToCheck);
+			return true;
+		}
+		catch(Exception e){
+			System.out.println(e);
+			
+		}
+		return result;
+	}
+	public static boolean checkIfNumberisDivisibleByFour(int numberToCheck) {
+		boolean result = false;
+		if(numberToCheck % 4 == 0)
+		{
+			result = true;
+		}
+		return result;
+	}
+	public static boolean checkIfNumberisDivisibleByHundred(int number) {
+		boolean result = false;
+		if(number % 100 == 0)
+		{
+			result = true;
+		}
+		return result;
+	}
+	public static boolean checkIfNumberisDivisibleByFourHundred(int number) {
+		boolean result = false;
+		if(number % 400 == 0)
+		{
+			result = true;
+		}
+		return result;
+	}
+	public static boolean checkIfLeapYear(String num) {
+		boolean result = true;
+		int number = Integer.parseInt(num);
+		if(!checkIfInputIsInteger(num)){
+			result = false;
+		}
+		else if(!checkIfNumberisDivisibleByFour(number) || (checkIfNumberisDivisibleByHundred(number) && !checkIfNumberisDivisibleByFourHundred(number))){
+			result = false;
+		}	
+		return result;
+	}
+}
---
+++ src/test/java/com/cengage/index/repo/LeapYearTester.java
@@ -0,0 +1,59 @@
+package com.cengage.index.repo;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.cengage.TDD.TDD.LeapYear;
+
+public class LeapYearTester {
+
+
+	@Test
+	public void verifyIfInputIsNumberWithValidInput() {
+		String year = "2000";
+		
+		Assert.assertTrue(LeapYear.checkIfInputIsInteger(year)); //This should Pass
+	}
+
+	@Test
+	public void verifyIfInputIsNumberWithInValidInput() {
+		String year = "hb";
+		Assert.assertFalse(LeapYear.checkIfInputIsInteger(year)); 	
+	}
+
+	@Test
+	public void verifyIfNumberIsDivisibleByFour() {
+		int number = 1900;
+		Assert.assertTrue(LeapYear.checkIfNumberisDivisibleByFour(number)); 	
+	}
+	
+	@Test
+	public void verifyIfNumberisNotDivisibleByFour() {
+		int number = 1901;
+		Assert.assertFalse(LeapYear.checkIfNumberisDivisibleByFour(number)); 	
+	}
+	
+	@Test
+	public void verifyIfNumberisDivisibleByHundred() {
+		int number = 100;
+		Assert.assertTrue(LeapYear.checkIfNumberisDivisibleByHundred(number)); 	
+	}
+	
+	@Test
+	public void verifyIfNumberisNotDivisibleByHundred() {
+		int number = 101;
+		Assert.assertFalse(LeapYear.checkIfNumberisDivisibleByHundred(number)); 	
+	}
+	
+	@Test
+	public void verifyIfNumberisDivisibleByFourHundred() {
+		int number = 400;
+		Assert.assertTrue(LeapYear.checkIfNumberisDivisibleByFourHundred(number)); 	
+	}
+	
+	@Test
+	public void verifyIfNumberisNotDivisibleByFourHundred() {
+		int number = 401;
+		Assert.assertFalse(LeapYear.checkIfNumberisDivisibleByFourHundred(number)); 	
+	}
+	
+}
